1e369fb2baeec541fedcc0bb5a5004aa0efb4d55
Nitin Somisetty <nitin.somisetty@gmail.com>
2026-04-16 14:54:28 +0000

    comments added

--- topology.py
+++ topology.py
@@ -6,28 +6,31 @@ from mininet.log import setLogLevel
 
 
 """
+
 SDN Path Tracing Tool - Custom Mininet Topology
 Creates: 3 switches (S1, S2, S3) in a linear chain + 4 hosts
   H1 -- S1 -- S2 -- S3 -- H4
          |           |
          H2          H3
+
 """
 
 
 class PathTracingTopo(Topo): 
-    # Defining a custom topology class that inherits from the Topo class provided by Mininet. This allows us to create a specific network topology for our SDN path tracing tool.
-    # 3-switch topology with 4 hosts.
+    # we defin a custom topology class that inherits from the Topo class provided by Mininet.
 
     def build(self):
-        # self is a reference to the topology object, which is used to add switches, hosts, and links.
-        # Create switches and assign dpid manually
 
+        # self is a reference to the topology object
+
+        # Create switches and assign dpid manually
         s1 = self.addSwitch('s1', dpid='0000000000000001')
         s2 = self.addSwitch('s2', dpid='0000000000000002')
         s3 = self.addSwitch('s3', dpid='0000000000000003')
 
-        h1 = self.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01') #assigning mac address and names
-        h2 = self.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
+#now lets store the layer 2 and layer 3 info to some random python var
+        h1 = self.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01') # assigning mac address and names
+        h2 = self.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')  # /24 is a network prefix, same as subnet mask
         h3 = self.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
         h4 = self.addHost('h4', ip='10.0.0.4/24', mac='00:00:00:00:00:04')
 
@@ -40,17 +43,18 @@ class PathTracingTopo(Topo):
         
 def run():
     setLogLevel('info')
-    topo = PathTracingTopo()
+    topo = PathTracingTopo() #* create an instance of the PathTracingTopo class, which defines our custom network topology.
     net = Mininet(
         topo=topo,
         controller=lambda name: RemoteController(name, ip='127.0.0.1', port=6653) #connect to the remote controller running on localhost at port 6653 ( RYU)
     )
     net.start()
     print("Your network has started!")
-    CLI(net)
+    CLI(net) #mininet terminal opens
     net.stop()
 
-topos = {'pathtracingtopo': PathTracingTopo}
+topos = {'pathtracingtopo': PathTracingTopo} # This line creates a dictionary called topos that maps the string 'pathtracingtopo' to the PathTracingTopo class. 
+#This allows us to specify our custom topology when running Mininet from the command line.
 
 if __name__ == '__main__':
     run()
